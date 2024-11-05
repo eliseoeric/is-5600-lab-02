9dd3c2d248ce91fb90fdb28073e9642ee99050f1
Banu1-coder <banuteja.deshaboina@slu.edu>
2024-11-05 22:26:08 +0000

    Added generateUserList function.

--- app.js
+++ app.js
@@ -7,3 +7,18 @@ const saveButton = document.getElementById("btnSave");
 
 console.log(stocksData)
 
+function generateUserList(users,stocks ){
+    const userList=document.querySelector('.user-list')
+    userList.innerHTML='';
+    users.map(({user,id})=>{
+        const listItem = 
+        document.createElement('li');
+        listItem.innerText = user.lastname + ',  '+ user.firstname;
+        listItem.setAttribute('id', id );
+        userList.appendChild(listItem)
+        userList.addEventListener('click', (event)=>
+        handleUserListClick(event,users,stocks));
+    });
+
+}
+
