f1a80ea6d3c4b06937c0d0a02d8c683e1a4021ea
Banu1-coder <banuteja.deshaboina@slu.edu>
2024-11-05 22:23:19 +0000

    committed addeventlistener.

--- app.js
+++ app.js
@@ -1,2 +1,9 @@
 /* add your code here */
+document.addEventListener('DOMContentLoaded', () => {
+    const stocksData = JSON.parse(stockContent);
+    const userData = JSON.parse(userContent);
+const deleteButton = document.getElementById("btnDelete");
+const saveButton = document.getElementById("btnSave");
+
+console.log(stocksData)
 
