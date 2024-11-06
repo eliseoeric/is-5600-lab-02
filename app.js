6078896c7d93053c399aefa9548e1774baafb3cf
Shyam-design634 <meghashyamdora.hanumanthu@slu.edu>
2024-11-06 00:50:10 +0000

    Added AddEventListener , generateUserList in app.js

--- app.js
+++ app.js
@@ -1,2 +1,24 @@
 /* add your code here */
 
+document.addEventListener('DOMContentLoaded', () => {
+    const stocksData = JSON.parse(stockContent);
+    const userData = JSON.parse(userContent);
+const deleteButton = document.getElementById("btnDelete");
+const saveButton = document.getElementById("btnSave");
+
+console.log(stocksData)
+
+function generateUserList(users,stocks ){
+    const userList=document.querySelector('.user-list')
+    userList.innerHTML='';
+    users.map(({user,id})=>{
+        const listItem = 
+        document.createElement('li');
+        listItem.innerText = user.lastname + ',  '+ user.firstname;
+        listItem.setAttribute('id', id );
+        userList.appendChild(listItem)
+        userList.addEventListener('click', (event)=>
+        handleUserListClick(event,users,stocks));
+    });
+
+}
